e7416647e92c5ccded3f47215dab1037a4aa0f0b
Leonardo Gutierrez Gomez <leoguti85@gmail.com>
2025-03-18 10:13:29 +0100

    readme

---
+++ dvc/README.m
@@ -0,0 +1,54 @@
+******* first ************************************
+Commit source files to Git
+$ git init
+$ git add src
+$ git commit -m "Initial commit"
+
+
+Initialized DVC repository.
+$ dvc init
+
+
+Commit created dvc related files to Git
+$ git add .dvc .dvcignore
+$ git commit -m "Initialize DVC"
+
+
+add diamonds.csv to DVC tracking:
+$ dvc add data/diamonds.csv
+
+Check dvc data file
+$ cat data/diamonds.csv.dvc
+
+
+$ git add data
+$ git commit -m "Add initial version of the dataset to DVC"
+
+*********second *************************************
+We change the size of the dataset concatenating it to itself
+
+Check changes
+$ dvc status
+
+
+We track changes
+$ dvc add data/diamonds.csv
+
+To capture the change to diamonds.csv.dvc, Git commit:
+$ git add data/diamonds.csv.dvc
+$ git commit -m "Add more rows to diamonds.csv"
+
+***********Third***************************
+Check git commit history
+$ git log --oneline
+
+
+Back to a Data version
+git checkout 1f407bc
+
+A good practice if create a new branch to ztrack the new changes
+$ git switch -c new_branch
+
+If yur rename the data in this branch, then you create a new version
+
+To be safe, whenever you call git checkout, you should always call dvc checkout as well.
